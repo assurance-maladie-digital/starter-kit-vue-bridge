3049b5084bdbf07d70c5538c8bdf0a213d541cb3
David FYON <davidfyon@gmail.com>
2023-07-28 15:44:32 +0200

    add axios plugin

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,13 @@
 		<v-btn variant="tonal"> Button </v-btn>
 	</div>
 </template>
+
+<script setup>
+const axios = useNuxtApp().$axios;
+
+onMounted(async () => {
+	axios.get('/api/hello').then((res) => {
+		console.log(res.data.api);
+	});
+});
+</script>
