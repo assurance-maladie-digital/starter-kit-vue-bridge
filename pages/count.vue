a0626776689ba9a594e2460f93ed8991889788bb
David FYON <davidfyon@gmail.com>
2023-07-31 16:58:45 +0200

    add vuex plugin + exemple

---
+++ pages/count.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		Clicked: {{ count }} times, count is {{ evenOrOdd }}.
+		<v-btn variant="tonal" @click="increment">+</v-btn>
+		<v-btn variant="tonal" @click="decrement">-</v-btn>
+		<v-btn variant="tonal" @click="incrementIfOdd">Increment if odd</v-btn>
+		<v-btn variant="tonal" @click="incrementAsync">Increment async</v-btn>
+	</div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const count = computed(() => store.state.count);
+const evenOrOdd = computed(() => store.getters.evenOrOdd);
+
+const increment = () => store.dispatch("increment");
+const decrement = () => store.dispatch("decrement");
+const incrementIfOdd = () => store.dispatch("incrementIfOdd");
+const incrementAsync = () => store.dispatch("incrementAsync");
+</script>
