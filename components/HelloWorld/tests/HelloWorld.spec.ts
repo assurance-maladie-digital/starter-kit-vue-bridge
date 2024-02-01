57bd1115daeeed6aec9df95ddff1797bbca0a859
David FYON <davidfyon@gmail.com>
2024-02-01 09:52:29 +0100

    add component ex + pass jest test

---
+++ components/HelloWorld/tests/HelloWorld.spec.ts
@@ -0,0 +1,14 @@
+// jest test
+import { shallowMount } from '@vue/test-utils'
+
+import HelloWorld from '../HelloWorld.vue'
+
+describe('it renders correctly', () => {
+	it('renders prop msg when passed', () => {
+		const msg = 'new message'
+		const wrapper = shallowMount(HelloWorld, {
+			props: { msg }
+		})
+		expect(wrapper.text()).toMatch(msg)
+	})
+});
