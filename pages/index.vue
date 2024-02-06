0bc2313cec7ba2a77e8b226dce4195d544b2af80
David FYON <davidfyon@gmail.com>
2024-02-06 16:10:29 +0100

    remove package

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,6 @@
 <template>
-	<PageContainer>
-		<VCard class="pa-8">
-			Contenu de la page.
-		</VCard>
-	</PageContainer>
-	<PageContainer size="m">
-		<VCard class="pa-8">
-			Contenu de la page.
-		</VCard>
-	</PageContainer>
-	<BackBtn/>
-	<div class="d-flex align-center">
-		<p class="mb-0 mr-1">
-			Patient n°<b>1970756541</b>
-		</p>
-
-		<CopyBtn
-			label="Copier le numéro de patient"
-			text-to-copy="1970756541"
-		/>
+	<div>
+		<HelloWorld/>
+		<HelloWorld parentMessage="Un titre"/>
 	</div>
-	<VAlert text="Test" type="success"/>
 </template>
-
-<script setup lang="ts">
-import {PageContainer, BackBtn, CopyBtn} from 'bridge-david';
-import {useDisplay} from 'vuetify'
-
-const {mobile} = useDisplay()
-
-onMounted(() => {
-	console.log(mobile.value) // false
-});
-</script>
