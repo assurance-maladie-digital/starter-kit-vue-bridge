2f6af0208c82ef733c3996bab5741366fb8d161a
David FYON <davidfyon@gmail.com>
2024-02-09 09:05:00 +0100

    import package + exemple

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-	<div>
-		<HelloWorld/>
-		<HelloWorld parentMessage="Un titre"/>
-	</div>
+	<HelloWorld msg="Un titre"/>
+	<PageContainer>
+		Test
+	</PageContainer>
 </template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {PageContainer} from "bridge-david";
+
+export default defineComponent({
+	components: {
+		PageContainer
+	}
+});
+</script>
