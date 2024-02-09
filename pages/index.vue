92dcdecebda101b1a3ba24e775d9d63eae5bd1e9
David FYON <davidfyon@gmail.com>
2024-02-09 09:08:45 +0100

    add package + exemple

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-	<div>
+	<PageContainer>
 		<HelloWorld msg="Un titre" />
 		<HelloWorldBis parent-message="Test depuis src" />
-	</div>
+	</PageContainer>
 </template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {PageContainer} from "bridge-david";
+
+export default defineComponent({
+	components: {
+		PageContainer
+	}
+});
+</script>
