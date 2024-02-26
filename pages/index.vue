aa64bd786082fbac42e561e5e9adb2910f072d25
David FYON <davidfyon@gmail.com>
2024-02-26 13:44:40 +0100

    update exemples

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
 	<PageContainer>
-		<HelloWorld msg="Un titre" />
-		<HelloWorldBis parent-message="Test depuis src" />
+		<h1 class="text-primary">Home</h1>
+		<HelloWorld msg="Un titre"/>
+		<HelloWorldBis parent-message="Test depuis src"/>
 	</PageContainer>
 </template>
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,6 @@
 <template>
 	<PageContainer>
+		<h1 class="text-primary">Home</h1>
 		<HelloWorld msg="Un titre"/>
 		<HelloWorldBis parent-message="Test depuis src"/>
 	</PageContainer>
