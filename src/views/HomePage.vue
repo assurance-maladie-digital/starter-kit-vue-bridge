1058dcf99ff4db94734b84e1564d2ce4dcf11794
David FYON <davidfyon@gmail.com>
2024-04-03 22:28:31 +0200

    add font + styles + update package

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,160 +5,158 @@
 		:navigation-items="navigationItems"
 	/>
 	<PageContainer>
-		<PageContainer>
-			<div>
-				<div class="bg-grey-lighten-1">
-					<h2>UserMenuBtn</h2>
-					<UserMenuBtn
-						additional-information="Information supplémentaire"
-						full-name="Virginie Beauchesne"
-					/>
-				</div>
-				<div class="py-4">
-					<h2>DialogBox</h2>
-					<VBtn
-						color="primary"
-						@click="dialog = true"
-					>
-						Afficher le composant
+		<div>
+			<div class="bg-grey-lighten-1">
+				<h2>UserMenuBtn</h2>
+				<UserMenuBtn
+					additional-information="Information supplémentaire"
+					full-name="Virginie Beauchesne"
+				/>
+			</div>
+			<div class="py-4">
+				<h2>DialogBox</h2>
+				<VBtn
+					color="primary"
+					@click="dialog = true"
+				>
+					Afficher le composant
+				</VBtn>
+
+				<DialogBox
+					v-model="dialog"
+					width="500px"
+					title="Enregistrement"
+					@cancel="dialog = false"
+					@confirm="dialog = false"
+				>
+					<p>Souhaitez-vous procéder à l’enregistrement ?</p>
+				</DialogBox>
+			</div>
+			<div class="bg-grey-lighten-1">
+				<h2>DataList</h2>
+				<DataList :items="dataListItems"></DataList>
+			</div>
+			<div class="ml-4">
+				<h2>DataListGroup</h2>
+				<DataListGroup :items="dataListGroupItems"></DataListGroup>
+			</div>
+			<div class="bg-grey-lighten-1">
+				<h2>FooterWrapper</h2>
+				<FooterWrapper>
+					<VBtn variant="tonal" class="mr-6">
+						Mentions légales
 					</VBtn>
 
-					<DialogBox
-						v-model="dialog"
-						width="500px"
-						title="Enregistrement"
-						@cancel="dialog = false"
-						@confirm="dialog = false"
-					>
-						<p>Souhaitez-vous procéder à l’enregistrement ?</p>
-					</DialogBox>
-				</div>
-				<div class="bg-grey-lighten-1">
-					<h2>DataList</h2>
-					<DataList :items="dataListItems"></DataList>
-				</div>
-				<div class="ml-4">
-					<h2>DataListGroup</h2>
-					<DataListGroup :items="dataListGroupItems"></DataListGroup>
-				</div>
-				<div class="bg-grey-lighten-1">
-					<h2>FooterWrapper</h2>
-					<FooterWrapper>
-						<VBtn variant="tonal" class="mr-6">
-							Mentions légales
-						</VBtn>
-
-						<VBtn variant="tonal" class="mr-6">
-							CGU
-						</VBtn>
-
-						<VBtn variant="tonal" inert>
-							Version 1.0.0
-						</VBtn>
-					</FooterWrapper>
-				</div>
-				<div class="ml-4">
-					<h2>SubHeader</h2>
-					<SubHeader
-						back-btn-text="Back"
-						title-text="Paul Dupont"
-						sub-title-text="1 69 08 75 125 456 75"/>
-				</div>
-				<div class="bg-grey-lighten-1 mt-4">
-					<h2>BackBtn</h2>
-					<BackBtn/>
-				</div>
-				<div class="mt-4">
-					<h2>France connect Btn</h2>
-					<FranceConnectBtn href="https://app.franceconnect.gouv.fr/"/>
-				</div>
-				<div class="bg-grey-lighten-1 mt-4">
-					<h2>CopyBtn</h2>
-					<CopyBtn
-						label="Copier le numéro de dossier"
-						text-to-copy="example-2"
-					/>
-				</div>
-				<div class="mt-4">
-					<h2>Logo</h2>
-					<Logo/>
-				</div>
-				<div class="bg-grey-lighten-1 mt-4 my-4">
-					<h2>HeaderLoading</h2>
-					<HeaderLoading width="300px" height="30px"/>
-				</div>
-				<div class="mt-4 my-4">
-					<h2>BackToTopBtn</h2>
-					<BackToTopBtn/>
-				</div>
-				<div class="bg-grey-lighten-1">
-					<h2>CookieBanner</h2>
-					<div class="d-flex flex-wrap align-center justify-center">
-						<CookieBanner
-							v-if="active"
-							cookies-route=""
-							@reject="active = false"
-							@accept="active = false"
-						/>
-
-						<VBtn
-							v-if="!active"
-							color="primary"
-							@click="active = true"
-						>
-							Réinitialiser
-						</VBtn>
-					</div>
-				</div>
-				<div class="mt-4 my-4">
-					<h2>Logo Brand Section</h2>
-					<LogoBrandSection
-						service-sub-title="Documentation du Design System"
-						service-title="Design System"
-					/>
-				</div>
-				<div class="mt-4 my-4 bg-grey-lighten-1">
-					<h2>ChipList</h2>
-					<ChipList :items="chipItems"/>
-				</div>
-				<div class="mt-4 my-4">
-					<h2>NotificationBar</h2>
-					<NotificationBar>
-						<template #action>
-							<VBtn
-								variant="outlined"
-								color="white"
-								@click="removeNotification"
-							>
-								2ème action
-							</VBtn>
-						</template>
-					</NotificationBar>
-					<VBtn
-						color="primary"
-						@click="createNotification"
-					>
-						Envoyer une notification
+					<VBtn variant="tonal" class="mr-6">
+						CGU
 					</VBtn>
-				</div>
-				<div class="mt-4 my-4 bg-grey-lighten-1">
-					<h2>MaintenancePage</h2>
-					<MaintenancePage/>
-				</div>
-				<div class="mt-4 my-4">
-					<h2>NotFoundPage</h2>
-					<NotFoundPage/>
-				</div>
-				<div class="mt-4 my-4 bg-grey-lighten-1">
-					<h2>ErrorPage</h2>
-					<ErrorPage
-						code="500"
-						message="La page ne fonctionne pas correctement."
-						page-title="Erreur serveur"
+
+					<VBtn variant="tonal" inert>
+						Version 1.0.0
+					</VBtn>
+				</FooterWrapper>
+			</div>
+			<div class="ml-4">
+				<h2>SubHeader</h2>
+				<SubHeader
+					back-btn-text="Back"
+					title-text="Paul Dupont"
+					sub-title-text="1 69 08 75 125 456 75"/>
+			</div>
+			<div class="bg-grey-lighten-1 mt-4">
+				<h2>BackBtn</h2>
+				<BackBtn/>
+			</div>
+			<div class="mt-4">
+				<h2>France connect Btn</h2>
+				<FranceConnectBtn href="https://app.franceconnect.gouv.fr/"/>
+			</div>
+			<div class="bg-grey-lighten-1 mt-4">
+				<h2>CopyBtn</h2>
+				<CopyBtn
+					label="Copier le numéro de dossier"
+					text-to-copy="example-2"
+				/>
+			</div>
+			<div class="mt-4">
+				<h2>Logo</h2>
+				<Logo/>
+			</div>
+			<div class="bg-grey-lighten-1 mt-4 my-4">
+				<h2>HeaderLoading</h2>
+				<HeaderLoading width="300px" height="30px"/>
+			</div>
+			<div class="mt-4 my-4">
+				<h2>BackToTopBtn</h2>
+				<BackToTopBtn/>
+			</div>
+			<div class="bg-grey-lighten-1">
+				<h2>CookieBanner</h2>
+				<div class="d-flex flex-wrap align-center justify-center">
+					<CookieBanner
+						v-if="active"
+						cookies-route=""
+						@reject="active = false"
+						@accept="active = false"
 					/>
+
+					<VBtn
+						v-if="!active"
+						color="primary"
+						@click="active = true"
+					>
+						Réinitialiser
+					</VBtn>
 				</div>
 			</div>
-		</PageContainer>
+			<div class="mt-4 my-4">
+				<h2>Logo Brand Section</h2>
+				<LogoBrandSection
+					service-sub-title="Documentation du Design System"
+					service-title="Design System"
+				/>
+			</div>
+			<div class="mt-4 my-4 bg-grey-lighten-1">
+				<h2>ChipList</h2>
+				<ChipList :items="chipItems"/>
+			</div>
+			<div class="mt-4 my-4">
+				<h2>NotificationBar</h2>
+				<NotificationBar>
+					<template #action>
+						<VBtn
+							variant="outlined"
+							color="white"
+							@click="removeNotification"
+						>
+							2ème action
+						</VBtn>
+					</template>
+				</NotificationBar>
+				<VBtn
+					color="primary"
+					@click="createNotification"
+				>
+					Envoyer une notification
+				</VBtn>
+			</div>
+			<div class="mt-4 my-4 bg-grey-lighten-1">
+				<h2>MaintenancePage</h2>
+				<MaintenancePage/>
+			</div>
+			<div class="mt-4 my-4">
+				<h2>NotFoundPage</h2>
+				<NotFoundPage/>
+			</div>
+			<div class="mt-4 my-4 bg-grey-lighten-1">
+				<h2>ErrorPage</h2>
+				<ErrorPage
+					code="500"
+					message="La page ne fonctionne pas correctement."
+					page-title="Erreur serveur"
+				/>
+			</div>
+		</div>
 	</PageContainer>
 	<FooterBar
 		hide-a11y-link
