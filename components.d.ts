b9d14b83944d8049004942486fff479916ea6883
adrien charrier <adrien.charrier@outlook.fr>
2024-04-03 14:52:18 +0200

    ♻️organize dependencies

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld/HelloWorld.vue')['default']
-    HelloWorldBis: typeof import('./src/components/HelloWorldBis.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
