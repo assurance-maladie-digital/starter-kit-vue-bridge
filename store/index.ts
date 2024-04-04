b9915d886cea31a6d4c30c4634776d0f68282b00
VachetVirginie <vachetvirginie@gmail.com>
2024-04-04 14:44:59 +0200

    fix notifications errors (#40)
    Co-authored-by: David FYON <davidfyon@gmail.com>

--- store/index.ts
+++ store/index.ts
@@ -1,3 +1,21 @@
 import { createStore } from 'vuex';
+import { notification } from '@cnamts/synapse-bridge/modules/notification';
+import VuexPersistence from 'vuex-persist';
 
-export default createStore({});
+import { RootState } from './types';
+
+const vuexLocal = new VuexPersistence<RootState>({
+	storage: window.sessionStorage,
+});
+
+const store = createStore({
+	state: {},
+	modules: {
+		notification
+	},
+	plugins: [
+		vuexLocal.plugin
+	]
+});
+
+export default store;
