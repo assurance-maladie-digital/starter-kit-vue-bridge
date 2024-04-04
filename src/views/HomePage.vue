9fd4afd5a7d87e86167ee547d7dbad008bc3266a
virginieVachet <vachetvirginie@gmail.com>
2024-04-04 10:21:50 +0200

    fix notifications errors

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,6 +6,26 @@
 	<PageContainer>
 		<h1 class="text-primary">Home</h1>
 		<HelloWorld msg="Un titre"/>
+		<div class="mt-4 my-4">
+			<h2>NotificationBar</h2>
+			<NotificationBar>
+				<template #action>
+					<VBtn
+						variant="outlined"
+						color="white"
+						@click="removeNotification"
+					>
+						2ème action
+					</VBtn>
+				</template>
+			</NotificationBar>
+			<VBtn
+				color="primary"
+				@click="createNotification"
+			>
+				Envoyer une notification
+			</VBtn>
+		</div>
 	</PageContainer>
 	<FooterBar
 		hide-a11y-link
@@ -20,13 +40,34 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import {PageContainer, HeaderBar, FooterBar} from "@cnamts/synapse-bridge";
-
+import {PageContainer, HeaderBar, NotificationBar, FooterBar} from "@cnamts/synapse-bridge";
+import { mapActions, mapGetters } from "vuex";
 export default defineComponent({
 	components: {
 		PageContainer,
 		HeaderBar,
+		NotificationBar,
 		FooterBar
+	},
+	computed: {
+		...mapGetters('notification', {
+			notification: 'notification',
+		}),
+	},
+	methods: {
+		...mapActions('notification', {
+			dispatchNotification: 'addNotification',
+			dispatchClearNotification: 'clearNotification',
+		}),
+		createNotification(): void {
+			this.dispatchNotification({
+				type: 'info',
+				message: 'Exemple de notification.',
+			})
+		},
+		removeNotification(): void {
+			this.dispatchClearNotification();
+		}
 	}
 });
 </script>
