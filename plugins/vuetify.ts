d7b27893907f3795183617c2347a7ac1ac4ea0b0
virginieVachet <vachetvirginie@gmail.com>
2024-06-04 17:37:02 +0200

    change fonts depending on theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,9 +2,10 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 import { colorTheme, colorBoostrapTheme } from '@cnamts/design-tokens/src/colors';
-
 export default defineNuxtPlugin(async (nuxtApp) => {
 	const theme = await $fetch<Record<string, string>>('/json/config.env.json').then((res) => res.theme);
+	const fontTheme = 'Source_Sans_3, sans-serif';
+	const fontBoostrapTheme =  'Tahoma, Helvetica neue, sans serif';
 	const vuetify = createVuetify({
 		ssr: true,
 		icons: {
@@ -23,6 +24,6 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 			},
 		},
 	});
-
+	document.body.style.fontFamily = theme === 'bootstrap' ? fontBoostrapTheme : fontTheme;
 	nuxtApp.vueApp.use(vuetify);
 });
