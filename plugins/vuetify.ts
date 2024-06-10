e3df1a2e6afe561da74b3addc58a490d596f628c
David FYON <davidfyon@gmail.com>
2024-06-10 10:45:21 +0200

    update package + tokens (#54)

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,7 @@
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
-import { colorTheme, colorBoostrapTheme } from '@cnamts/design-tokens/src/colors';
+import { colorTheme, colorBootstrapTheme } from '@cnamts/design-tokens/src/colors';
 export default defineNuxtPlugin(async (nuxtApp) => {
 	const theme = await $fetch<Record<string, string>>('/json/config.env.json').then((res) => res.theme);
 	const vuetify = createVuetify({
@@ -17,7 +17,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 			defaultTheme: 'light',
 			themes: {
 				light: {
-					colors: theme === 'bootstrap' ? colorBoostrapTheme : colorTheme,
+					colors: theme === 'bootstrap' ? colorBootstrapTheme : colorTheme,
 				},
 			},
 		},
