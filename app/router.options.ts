b81d8b6fb0f869eab3ec047a58fd7d43d545345f
David FYON <davidfyon@gmail.com>
2024-06-17 10:38:36 +0200

    fix router

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,37 +1,25 @@
-import type { RouterConfig } from '@nuxt/schema'
+/**
+ * This file is used to use a vue-router configuration file to define the routes
+ * of the application. If you want to only use the nuxt default routing system,
+ * you can remove this file.
+ */
+import type { RouterConfig } from "@nuxt/schema";
+import routes from "@/router"; // Update to match your router configuration file path
 
-export default <RouterConfig> {
+export default <RouterConfig>{
 	// https://router.vuejs.org/api/interfaces/routeroptions.html#routes
-	routes: (_routes) => [
-		{
-			name: 'index',
-			path: '/',
-			component: () => import('~/src/views/Home.vue').then(r => r.default || r)
-		},
-		{
-			name: 'home',
-			path: '/home',
-			component: () => import('~/src/views/Home.vue').then(r => r.default || r)
-		},
-		{
-			name: 'tokens',
-			path: '/tokens',
-			component: () => import('~/src/views/Tokens.vue').then(r => r.default || r)
-		},
-		{
-			name: 'maintenance',
-			path: '/maintenance',
-			component: () => import('~/src/views/Maintenance.vue').then(r => r.default || r)
-		},
-		{
-			name: 'not-found',
-			path: '/not-found',
-			component: () => import('~/src/views/NotFound.vue').then(r => r.default || r)
-		},
-		{
-			name: 'test-inva',
-			path: '/test-inva',
-			component: () => import('~/src/views/TestInva.vue').then(r => r.default || r)
-		},
-	],
-}
+	routes: _routes => routes
+
+	/**
+	 * If you want to use the Nuxt default routing system in addition to the
+	 * vue-router configuration, you can use the following code:
+	 * ```
+	 * routes: (_routes) => {
+	 * 	return [
+	 * 		..._routes,
+	 * 		...routes
+	 * 	]
+	 * }
+	 * ```
+	 */
+};
