121887357fbf7ccea70e9692dffddd662e9ecb58
David FYON <davidfyon@gmail.com>
2024-06-19 13:44:28 +0200

    remove theming (#58)

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,7 @@ const routes = [
 		path: '/',
 		name: 'home',
 		component: () => import('@/views/HomePage.vue')
-	},
-	{
-		path: '/test-inva',
-		name: 'about',
-		component: () => import('@/views/TestInva.vue')
-	},
+	}
 ];
 
 export default routes;
