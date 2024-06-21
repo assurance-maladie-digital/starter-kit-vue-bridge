c9c04c82280a8a6a25077967ba2a7a2af23a2957
David FYON <davidfyon@gmail.com>
2024-06-21 09:52:22 +0200

    delete plugin (#61)

--- plugins/synapse.ts
+++
@@ -1,8 +0,0 @@
-import * as synapse from '@cnamts/synapse-bridge';
-import { defineNuxtPlugin } from 'nuxt/app';
-
-export default defineNuxtPlugin((nuxtApp) => {
-	for (const component in synapse) {
-		nuxtApp.vueApp.component(component, synapse[component]);
-	}
-});
