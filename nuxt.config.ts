7363f02b0d182eafe9074b9292778577eeaea472
virginieVachet <vachetvirginie@gmail.com>
2024-06-26 17:25:48 +0200

    fix defineNuxtConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 import {colorTheme} from "@cnamts/design-tokens/src/colors";
 import Components from "unplugin-vue-components/vite";
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
 	ssr: false,
@@ -14,7 +15,8 @@ export default defineNuxtConfig({
 	modules: [
 		"vuetify-nuxt-module",
 		'dayjs-nuxt',
-		'@nuxtjs/i18n'
+		'@nuxtjs/i18n',
+		'vuetify-nuxt-module'
 	],
 	plugins: [
 		'~/plugins/vuex.ts',
