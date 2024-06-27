1222dec1ce356884feaa85670757294b67e21387
virginieVachet <vachetvirginie@gmail.com>
2024-06-27 15:45:29 +0200

    add counter store tests

--- store/counter.ts
+++ store/counter.ts
@@ -1,19 +1,19 @@
-import { createStore } from 'vuex';
+import {createStore} from 'vuex';
 
 const counter = createStore({
-  state: () => ({
-	count: 0
-  }),
-  mutations: {
-	  increment(state) {
-		  state.count++;
-	  },
-	  decrement(state) {
-		  state.count--;
-	  }
-  },
+	state: () => ({
+		count: 0
+	}),
+	mutations: {
+		increment(state) {
+			state.count++;
+		},
+		decrement(state) {
+			state.count--;
+		}
+	},
 	actions: {
-	  		increment({commit}) {
+		increment({commit}) {
 			commit('increment');
 		},
 		decrement({commit}) {
