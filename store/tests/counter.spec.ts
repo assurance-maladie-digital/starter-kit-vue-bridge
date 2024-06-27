3b105a48196539394fd582de57fe0aa16d30ec88
virginieVachet <vachetvirginie@gmail.com>
2024-06-27 15:45:29 +0200

    add counter store tests

---
+++ store/tests/counter.spec.ts
@@ -0,0 +1,36 @@
+import { createStore } from 'vuex';
+import counter from '../counter';
+
+describe('Counter Store', () => {
+	let store;
+
+	beforeEach(() => {
+		store = counter;
+	});
+
+	test('initial state', () => {
+		expect(store.state.count).toBe(0);
+	});
+
+	test('increment mutation', () => {
+		store.commit('increment');
+		expect(store.state.count).toBe(1);
+	});
+
+	test('decrement mutation', () => {
+		store.commit('increment');
+		store.commit('decrement');
+		expect(store.state.count).toBe(0);
+	});
+
+	test('increment action', async () => {
+		await store.dispatch('increment');
+		expect(store.state.count).toBe(1);
+	});
+
+	test('decrement action', async () => {
+		await store.dispatch('increment');
+		await store.dispatch('decrement');
+		expect(store.state.count).toBe(0);
+	});
+});
