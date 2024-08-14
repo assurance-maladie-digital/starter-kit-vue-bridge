5a081b6dc3ec6a69f639b416990fbeef220b29a5
virginieVachet <vachetvirginie@gmail.com>
2024-08-14 15:26:02 +0200

    improve home tests

--- src/views/tests/Home.spec.ts
+++ src/views/tests/Home.spec.ts
@@ -3,7 +3,7 @@ import Home from '../Home.vue'
 import { vuetify } from '../../../tests/unit/setup'
 import notifications from '../../stores/notifications'
 import axios from 'axios'
-import {Store} from "vuex";
+import type {Store} from "vuex";
 
 describe('home page should render', () => {
 	it('renders title when config is provided', () => {
