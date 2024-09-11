e611469ee824a174c31d631c5d1231d5c8a4a035
adrien charrier <adrien.charrier@outlook.fr>
2024-09-11 16:33:54 +0200

    chore: update eslint configuration to use esmodule

--- .eslintrc.migration.cjs
+++ .eslintrc.migration.cjs
@@ -9,7 +9,7 @@ module.exports = {
 		parser: '@typescript-eslint/parser',
 	},
 	extends: [
-		'@nuxtjs/eslint-config-typescript',
+		//'@nuxtjs/eslint-config-typescript',
 		'plugin:vue/base',
 		'plugin:vuetify/base',
 	],
